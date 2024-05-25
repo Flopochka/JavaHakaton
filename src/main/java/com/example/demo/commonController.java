939b25056b0df0215c078ffe36d7a40235458225
Flopochka <ym.uvtqk@gmail.com>
2024-05-25 08:03:37 +0300

    фыф

---
+++ src/main/java/com/example/demo/commonController.java
@@ -0,0 +1,29 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private TextField EmailInput;
+
+    @FXML
+    private Button GuestBtn;
+
+    @FXML
+    private Button LoginBtn;
+
+    @FXML
+    private PasswordField PasswordInput;
+
+    @FXML
+    private Button Registerbtn;
+
+    @FXML
+    void initialize() {
+        LoginBtn.setOnAction(event -> {
+            if ()
+        });
+    }
+}
